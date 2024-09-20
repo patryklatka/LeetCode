da1485307133dd60f0689f2f00485da84a45f1b7
Patryk Latka <patryk.latka@op.pl>
2024-09-20 23:11:46 +0200

    Solution for problem: Longest Palindromic Substring

---
+++ C_exercises/5_Longest_Palindromic_Substring/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool check(char* s, int i, int j) {
+    int left = i;
+    int right = j - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+char* longestPalindrome(char* s) {
+    int length = strlen(s);
+    char* result = strdup("");
+
+    for (int len = length; len > 0; len--) {
+        for (int start = 0; start <= strlen(s) - len; start++) {
+            if (check(s, start, start + len)) {
+                free(result);
+                result = strndup(s + start, len);
+                return result;
+            }
+        }
+    }
+
+    return result;
+}
