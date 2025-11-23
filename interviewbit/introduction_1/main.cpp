0cf096475f4dbc35f677fbcdaa6b51c49d025931
Patryk Latka <patryk.latka@op.pl>
2025-11-23 23:59:57 +0100

    Start C++ course from interviebit: 1st and 2nd exercise

---
+++ interviewbit/introduction_1/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+
+    cout <<  "Hello, InterviewBit!"  << endl;
+    
+    return 0;
+}
---
+++ interviewbit/introduction_2/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    char ch;
+    cin>>ch;
+    //Your code goes here
+    int i = int(ch);
+    cout << i << endl;
+    return 0;
+}
